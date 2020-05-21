d01f396c209109c8c89f42378f7ce5e66274ef0f
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-21 23:51:26 +0530

    SImplify code

--- selection-sort/selectionsort_cpp.cpp
+++ selection-sort/selectionsort_cpp.cpp
@@ -1,7 +1,4 @@
 //
-//  main.cpp
-//  stoofcpp
-//
 //  Created by Shrish Deshpande on 5/16/20.
 //  Copyright © 2020 Shrish Deshpande. All rights reserved.
 //
@@ -9,30 +6,31 @@
 #include<iostream>
 using namespace std;
 
-void display(int *array, int size) {
-   for(int i = 0; i<size; i++)
-      cout << array[i] << " ";
-   cout << endl;
-}
-void sort(int *array, int size) {
-   int i, j, imin;
-   for(i = 0; i<size-1; i++) {
-      imin = i;
-      for(j = i+1; j<size; j++)
-         if(array[j] < array[imin])
-            imin = j;
-         swap(array[i], array[imin]);
-   }
-}
 int main() {
-   int length;
-   cout << "Length" << endl;
-   cin >> length;
-   int arr[length];
-   cout << "Elements" << endl;
-    for(int i = 0; i < length; i++) {
-      cin >> arr[i];
-   }
-   sort(arr, length);
-   display(arr, length);
+  //Input
+  int length;
+  cout << "Enter Length\n";
+  cin >> length;
+  int arr[length];
+  cout << "Enter the Elements\n";
+  for(int i = 0; i < length; i++) {
+    cin >> arr[i];
+  }
+
+  //Sorting
+  int i, j, imin;
+  for(i = 0; i<length-1; i++) {
+    imin = i;
+    for(j = i+1; j<length; j++) {
+      if(arr[j] < arr[imin]) {
+        imin = j;
+      }
+    }
+    swap(arr[i], arr[imin]);
+  }
+
+  //Printing
+  for(int i = 0; i<length; i++)
+    cout << arr[i] << " ";
+  cout << endl;
 }
